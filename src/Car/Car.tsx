201857caebcbbc3134a98cd796f6c682ef7c4339
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2023-02-08 10:11:39 +0300

    conditional rendering

--- src/Car/Car.tsx
+++ src/Car/Car.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {CSSProperties} from "react";
 import './Car.css';
 
 //  function Car() {
@@ -14,15 +14,28 @@ import './Car.css';
 //     </div>
 // )
 
-const car = (props: any) => (
-    <div className="Car">
-        <h3>Car name: {props.name}</h3>
-        <p>Year: {props.year}</p>
-        <input type="text" onChange={props.onChangeCarNameHandler} value={props.name}/>
-        <button onClick={props.onChangeTitle}>Set Title</button>
-        <button onClick={props.onDelete}>Delete</button>
-    </div>
-)
+const car = (props: any) => {
+        const inputClasses: string[] = ['Input'];
+
+        if (props.name === '') inputClasses.push('red');
+        if (props.name !== '') inputClasses.push('green');
+        if (props.name.length > 4) inputClasses.push('bold');
+
+        return (
+            <div className="Car">
+                    <h3>Car name: {props.name}</h3>
+                    <p>Year: {props.year}</p>
+                    <input type="text"
+                           onChange={props.onChangeCarNameHandler}
+                           value={props.name}
+                           className={inputClasses.join(' ')}
+                    />
+                    <button onClick={props.onChangeTitle}>Set Title</button>
+                    <button onClick={props.onDelete}>Delete</button>
+            </div>
+        )
+
+}
 
 export default car
 
