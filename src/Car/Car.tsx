2bdb64c2dfbfc86cfabbe58e2e48a1c08f3ee9d8
Vladislav Kulichkov <kulichkov.asm2005@asugubkin.ru>
2023-02-08 10:34:52 +0300

    styles with radium

--- src/Car/Car.tsx
+++ src/Car/Car.tsx
@@ -1,5 +1,6 @@
 import React, {CSSProperties} from "react";
 import './Car.css';
+import Radium from 'radium';
 
 //  function Car() {
 //  return (
@@ -14,29 +15,39 @@ import './Car.css';
 //     </div>
 // )
 
-const car = (props: any) => {
-        const inputClasses: string[] = ['Input'];
+const Car = (props: any) => {
+    const inputClasses: string[] = ['Input'];
 
-        if (props.name === '') inputClasses.push('red');
-        if (props.name !== '') inputClasses.push('green');
-        if (props.name.length > 4) inputClasses.push('bold');
+    if (props.name === '') inputClasses.push('red');
+    if (props.name !== '') inputClasses.push('green');
+    if (props.name.length > 4) inputClasses.push('bold');
 
-        return (
-            <div className="Car">
-                    <h3>Car name: {props.name}</h3>
-                    <p>Year: {props.year}</p>
-                    <input type="text"
-                           onChange={props.onChangeCarNameHandler}
-                           value={props.name}
-                           className={inputClasses.join(' ')}
-                    />
-                    <button onClick={props.onChangeTitle}>Set Title</button>
-                    <button onClick={props.onDelete}>Delete</button>
-            </div>
-        )
+    const style = {
+        border: '1px solid black',
+        boxShadow: '0 4px 5px 0 black',
+        ':hover': {
+            border: '1px solid white',
+            boxShadow: '0 6px 7px 8px green',
+            cursor: 'pointer'
+        }
+    }
+
+    return (
+        <div className="Car" style={style}>
+            <h3>Car name: {props.name}</h3>
+            <p>Year: {props.year}</p>
+            <input type="text"
+                   onChange={props.onChangeCarNameHandler}
+                   value={props.name}
+                   className={inputClasses.join(' ')}
+            />
+            <button onClick={props.onChangeTitle}>Set Title</button>
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    )
 
 }
 
-export default car
+export default Radium(Car)
 
 
